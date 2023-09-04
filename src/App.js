4d2cc19686d423682c5bb0e15ce9f42fc815dcd7
MenendezON <menendezon@gmail.com>
2023-09-04 00:44:37 +0000

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Navigation';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
