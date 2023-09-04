3708417bc903f77dd115011cbcdad8947a898111
MenendezON <menendezon@gmail.com>
2023-09-04 00:34:01 +0000

    Createa home and details file

---
+++ src/pages/Details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Details() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the Details page!</p>
+    </div>
+  );
+}
+
+export default Details;
