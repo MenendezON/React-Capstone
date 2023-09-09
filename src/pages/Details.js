f3aec3743f884e0c3a39a571cb227050cf9f8c2f
MenendezON <menendezon@gmail.com>
2023-09-09 04:26:40 +0000

    update pages content

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Headers from '../components/Header';
 import SpecDetails from '../components/SpecDetails';
+import { fetchStatsAsync } from '../redux/stats/statsSlice';
 
 function Details() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchStatsAsync());
+  });
   return (
     <div>
       <Headers title="Content view" back="true" />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import Dragons from '../components/dragons';
-import { fetchDragonsAsync } from '../redux/dragons/dragonsSlice';
+import Stats from '../components/StatAll';
+import { fetchStatsAsync } from '../redux/stats/statsSlice';
 import Headers from '../components/Header';
 
 function Home() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchDragonsAsync());
+    dispatch(fetchStatsAsync());
   });
   return (
     <div>
       <Headers title="Years view" back="false" />
-      <Dragons />
+      <Stats />
     </div>
   );
 }
