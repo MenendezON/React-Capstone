9e8cc9148fc4609ed9c4ff782fc2b48daf227b9c
MenendezON <menendezon@gmail.com>
2023-09-09 19:14:47 +0000

    fix a capitalize error

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import React from 'react';
-import store from '../Redux/store';
+import store from '../redux/store';
 import App from '../App';
 
 describe('App js testing', () => {
