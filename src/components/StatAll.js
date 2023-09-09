87b49eed213e7a878ac646298c35d31263e7c273
MenendezON <menendezon@gmail.com>
2023-09-09 17:45:39 +0000

    Update capstone project

--- src/components/StatAll.js
+++ src/components/StatAll.js
@@ -3,9 +3,15 @@ import { useSelector } from 'react-redux';
 import {
   Link,
 } from 'react-router-dom';
+import imgAvatar from '../assets/money-1425580_1920.png';
 
 const Stats = () => {
   const stats = useSelector((state) => state.stats.stats);
+
+  function shortRevenue(revenue) {
+    return (parseInt(revenue, 10) / 1000000000).toFixed(2);
+  }
+
   const [filter, setFilter] = useState('All');
 
   const tab = [{ id: 0, name: 'All' }];
@@ -16,9 +22,10 @@ const Stats = () => {
     })
   ));
 
-  function shortRevenue(revenue) {
-    return (parseInt(revenue, 10) / 1000000000).toFixed(2);
-  }
+  const searchedStats = stats.filter((data) => {
+    const { calendarYear } = data;
+    return calendarYear.includes(filter);
+  });
 
   return (
     <>
@@ -26,7 +33,7 @@ const Stats = () => {
         {tab.map((option) => (
           <button
             type="button"
-            onClick={() => { setFilter(option.name); }}
+            onClick={() => setFilter(option.name)}
             key={option.id}
             style={{ backgroundColor: filter === option.name ? '#ce4277' : 'white', color: filter === option.name ? 'white' : 'black' }}
           >
@@ -35,12 +42,12 @@ const Stats = () => {
         ))}
       </ul>
       <section className="cards">
-
-        {stats.length > 0 && filter === 'All' && stats.map((stat) => (
+        {searchedStats.map((stat) => (
           <Link to={`/details/${stat.calendarYear}`} key={stat.id} className="card">
-            <div>
-              <p>{stat.calendarYear}</p>
-              <h3>{stat.symbol}</h3>
+            <div className="avatar">
+              <img src={imgAvatar} alt="" />
+            </div>
+            <div className="desc">
               <h2>
                 $
                 { shortRevenue(stat.revenue)}
@@ -51,17 +58,19 @@ const Stats = () => {
             </div>
           </Link>
         ))}
-        {stats.length > 0 && filter !== 'All' && stats.filter((stat) => stat.calendarYear === filter).map((stat) => (
+        {filter === 'All' && stats.length > 0 && stats.map((stat) => (
           <Link to={`/details/${stat.calendarYear}`} key={stat.id} className="card">
-            <div>
-              <p>{stat.calendarYear}</p>
-              <h3>{stat.symbol}</h3>
+            <div className="avatar">
+              <img src={imgAvatar} alt="" />
+            </div>
+            <div className="desc">
               <h2>
                 $
                 { shortRevenue(stat.revenue)}
                 {' '}
                 bills
               </h2>
+              <p>{stat.date}</p>
             </div>
           </Link>
         ))}
