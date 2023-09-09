4585a2bd5cdfe778eeba280c2561444702efc0e8
MenendezON <menendezon@gmail.com>
2023-09-09 04:28:52 +0000

    fix linters

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/details/:id" element={<Details />} />
+        <Route path="/details/:yearToFilter" element={<Details />} />
       </Routes>
     </>
   );
